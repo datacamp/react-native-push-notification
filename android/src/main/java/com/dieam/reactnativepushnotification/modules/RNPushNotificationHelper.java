cdd10fd7fcfa238533a9f6079e05a2ce8eef687d
Thomas Corthouts <thomas.corthouts@icloud.com>
2018-07-25 21:21:13 +0200

    Change priority (#1)

--- android/src/main/java/com/dieam/reactnativepushnotification/modules/RNPushNotificationHelper.java
+++ android/src/main/java/com/dieam/reactnativepushnotification/modules/RNPushNotificationHelper.java
@@ -156,12 +156,34 @@ public class RNPushNotificationHelper {
                 ApplicationInfo appInfo = context.getApplicationInfo();
                 title = context.getPackageManager().getApplicationLabel(appInfo).toString();
             }
+            NotificationCompat priority;
+            final String priorityString = bundle.getString("priority");
+            if (priorityString != null) {
+                switch(priorityString.toLowerCase()) {
+                    case "max":
+                        priority = NotficationCompat.PRIORITY_MAX;
+                        break;
+                    case "high":
+                        priority = NotficationCompat.PRIORITY_HIGH;
+                        break;
+                    case "low":
+                        priority = NotficationCompat.PRIORITY_LOW;
+                        break;
+                    case "min":
+                        priority = NotficationCompat.PRIORITY_MIN;
+                        break;
+                    default:
+                        priority = NotificationCompat.PRIORITY_LOW
+                }
+            } else {
+                priority = NotificationCompat.PRIORITY_LOW;
+            }
 
             NotificationCompat.Builder notification = new NotificationCompat.Builder(context)
                     .setContentTitle(title)
                     .setTicker(bundle.getString("ticker"))
                     .setVisibility(NotificationCompat.VISIBILITY_PRIVATE)
-                    .setPriority(NotificationCompat.PRIORITY_HIGH)
+                    .setPriority(priority)
                     .setAutoCancel(bundle.getBoolean("autoCancel", true));
 
             String group = bundle.getString("group");
